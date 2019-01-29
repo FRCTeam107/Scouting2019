04694dbd0fe10db654429a9487dcf87d68f48a82
David Reidsma <david.reidsma@gmail.com>
2019-01-29 12:59:19 -0500

    quack

---
+++ app/src/main/java/com/frc107/scouting2019/DuckActivity.java
@@ -0,0 +1,48 @@
+package com.frc107.scouting2019;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.frc107.scouting2019.view.MainActivity;
+import com.frc107.scouting2019.view.PitActivity;
+import com.frc107.scouting2019.view.ScouterInitialsActivity;
+import com.frc107.scouting2019.view.SendDataActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class DuckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duck);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main_activity:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            case R.id.send_data:
+                startActivity(new Intent(this, SendDataActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
