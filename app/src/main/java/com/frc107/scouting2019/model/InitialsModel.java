cac3e924d768cb3ae5ac72c974a788c1ce7462eb
David Reidsma <david.reidsma@gmail.com>
2019-03-20 21:42:45 -0400

    fixed empty initials crash

--- app/src/main/java/com/frc107/scouting2019/model/InitialsModel.java
+++ app/src/main/java/com/frc107/scouting2019/model/InitialsModel.java
@@ -5,6 +5,10 @@ import com.frc107.scouting2019.Scouting;
 public class InitialsModel {
     private String initials;
 
+    public InitialsModel() {
+        initials = "";
+    }
+
     public void setInitials(String initals) {
         this.initials = initals;
         Scouting.getInstance().setInitials(initials);
